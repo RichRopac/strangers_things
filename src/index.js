cbe30f15ec3321279d4d1e96fadf29f5d52aedf8
rcropac <rcropac@gmail.com>
2022-07-06 10:22:52 -0500

    Second

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import {
     BrowserRouter,
     Routes,
-    Route,
-
+    Route, 
+    Link
 } from "react-router-dom"
 import { App } from './components'
 const root = ReactDOM.createRoot(document.getElementById('app'))

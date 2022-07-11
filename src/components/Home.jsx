c9c8a0651ccb791e45b7db75822a8ede1c1618da
rcropac <rcropac@gmail.com>
2022-07-10 20:16:37 -0500

    Sunday Update 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./Home.css"
 
 export const Home = () => {
-return <span className="title">Stranger's Things</span>
+return (
+      
+        
+        <div className="homePage">Stranger's Things</div>
+      
+ 
+    )
 
 }
